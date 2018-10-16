59b0a39178ff311b72b7d7174cb4ec93dd62d1b0
wsl <wangsuoling@163.com>
2018-10-16 22:59:42 +0800

    优化client

--- client/client.js
+++ client/client.js
@@ -26,43 +26,108 @@ var request = require('request');
 //     }
 // });
 
-inquirer.prompt(questions.ifLoginQuestion).then(ifLoginAnswers => {
-  if (ifLoginAnswers.event === 'login') {
-    var url='http://localhost:3000/login';
-    inquirer.prompt(questions.loginQuestion).then(loginAnswers => {
-      console.log(loginAnswers)
-      request({
-        url: url,
-        method: 'POST',
-        json: true,
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: loginAnswers
-      }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // 请求成功的处理逻辑
-        }
-      });
-    })
-  } else if (ifLoginAnswers.event === 'register') {
-    var url='http://localhost:3000/register';
-    inquirer.prompt(questions.registerQuestion).then(registerAnswers => {
-      console.log(registerAnswers)
-      request({
-        url: url,
-        method: 'POST',
-        json: true,
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: registerAnswers
-      }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // 请求成功的处理逻辑
-        }
-      });
-    })
+
+function requestFun (url, sendData) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: url,
+      method: 'POST',
+      json: true,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: sendData
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body)
+        // console.log(body) // 请求成功的处理逻辑
+      } else  {
+        reject(error || response.statusCode)
+      }
+    });
+  })
+}
+
+// async function userLogin(){
+//   let userInput = await inquirer.prompt(questions.ifLoginQuestion)
+//   let requestResult = await requestFun(userInput)
+//   return requestresult
+// }
+
+// userLogin.then()
+// async function ifLogin (questions) {
+//   return await inquirer.prompt(questions)
+// }
+// ifLogin(questions.ifLoginQuestion).then((result) => {
+//   console.log(result)
+// })
+// var ifLoginAnswers = await ifLogin()
+// if (ifLoginAnswers.event === 'login') {
+//   var url='http://localhost:3000/login';
+//   // var loginAnswers = await inquirer.prompt(questions.loginQuestion)
+//   // requestFun(url, loginAnswers)
+  
+// } else if (ifLoginAnswers.event === 'register') {
+//   var url='http://localhost:3000/register';
+//   // var registerAnswers = await inquirer.prompt(questions.registerQuestion)
+//   // requestFun(url, registerAnswers)
+// }
+
+// inquirer.prompt(questions.ifLoginQuestion).then(ifLoginAnswers => {
+//   if (ifLoginAnswers.event === 'login') {
+//     var url='http://localhost:3000/login';
+//     inquirer.prompt(questions.loginQuestion).then(loginAnswers => {
+//       console.log(loginAnswers)
+//       request({
+//         url: url,
+//         method: 'POST',
+//         json: true,
+//         headers: {
+          
+//             'content-type': 'application/json',
+//         },
+//         body: loginAnswers
+//       }, function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body) // 请求成功的处理逻辑
+//         }
+//       });
+//     })
+//   } else if (ifLoginAnswers.event === 'register') {
+//     var url='http://localhost:3000/register';
+//     inquirer.prompt(questions.registerQuestion).then(registerAnswers => {
+//       console.log(registerAnswers)
+//       request({
+//         url: url,
+//         method: 'POST',
+//         json: true,
+//         headers: {
+//             'content-type': 'application/json',
+//         },
+//         body: registerAnswers
+//       }, function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             console.log(body) // 请求成功的处理逻辑
+//         }
+//       });
+//     })
+//   }
+//   // console.log(JSON.stringify(answers, null, '  '));
+// });
+
+async function main (){
+  let result = await inquirer.prompt(questions.ifLoginQuestion)
+  if(result.event === 'login'){
+    result = await inquirer.prompt(questions.loginQuestion)
+    result = await requestFun('http://localhost:3000/login', result)
+
+  } else if (result.event === 'register') {
+    result = await inquirer.prompt(questions.registerQuestion)
+    result = await requestFun('http://localhost:3000/register', result)
   }
-  // console.log(JSON.stringify(answers, null, '  '));
-});
+  return result
+}
+
+main().then(res => {
+  console.log(res)
+})
